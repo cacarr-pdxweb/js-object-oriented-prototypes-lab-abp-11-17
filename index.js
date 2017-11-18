a791e5f1f145f4e554968fffe93d59e18306a8b6
Christopher Carr <christopher.andrew.carr@gmail.com>
2017-11-18 18:17:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,30 +25,30 @@
 // }
 
 function BoardMember(name, homeState, training){
-  this.name = name
-  this.homeState = homeState
-  this.training = training
+  this.name = name;
+  this.homeState = homeState;
+  this.training = training;
   
 }
 
 BoardMember.prototype.veto = function(){
-  return 'No, I must disagree'
-}
+  return 'No, I must disagree';
+};
 
 BoardMember.prototype.approve = function(){
-  return 'You can do that!'
-}
+  return 'You can do that!';
+};
 
 BoardMember.prototype.doCharity = function(){
-  return "I like to help people"
-}
+  return "I like to help people";
+};
 
 BoardMember.prototype.releasePressStatement(){
-  return "You will see great things from Scuber."
+  return "You will see great things from Scuber.";
 }
 
 BoardMember.prototype.sayHi(){
-  return `Hi, my name is ${this.name}. I am from ${this.state}, and I was trainted in ${this.training}.`
-  
+  return `Hi, my name is ${this.name}. I am from ${this.state}, and I was trainted in ${this.training}.`;
+}
   
   
