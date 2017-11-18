1fdcfb530f7db403df168582a471b08f7b468f7f
Christopher Carr <christopher.andrew.carr@gmail.com>
2017-11-18 18:10:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ BoardMember.prototype.approve = function(){
   return 'You can do that!'
 }
 
-BoardMember.prototype.approve = function(){
-  return doCharity = 
+BoardMember.prototype.doCharity = function(){
+  return "I like to help people"
 }
+
